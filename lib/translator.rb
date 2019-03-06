173c696b35471edf729698a62787a662fb7022fa
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-06 21:26:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,13 @@
 require "yaml"# require modules here
 require 'fry'
+
 def load_library(emoticon_file)
   library = YAML.load_file(emoticon_file)
   result = {"get_meaning" => {}, "get_emoticon" => {}}
   library.each do |meaning, emoticons|
+    binding.pry
     result["get_meaning"][emoticons[1]] = meaning
     result["get_emoticon"][emoticons[0]] = emoticons[1]
-    binding.pry
   end
   result
 
