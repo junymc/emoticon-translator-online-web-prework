42a44dc6483d5a882ecccd9d48d3eed0983ee15f
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-06 21:58:11 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,39 @@
-# require modules here
+require "yaml"# require modules here
+# emoticon_file = {
+# angel:
+#   - "O:)"
+#   - "☜(⌒▽⌒)☞"
+# angry:
+#   - ">:("
+#   - "ヽ(ｏ`皿′ｏ)ﾉ"
+# }
 
-def load_library
-  # code goes here
+# result = {"get_meaning" => {"☜(⌒▽⌒)☞":angel}, "get_emoticon" => {"O:)":"☜(⌒▽⌒)☞"}}
+
+def load_library(emoticon_file)
+    library = YAML.load_file(emoticon_file)
+    result = {"get_meaning" => {}, "get_emoticon" => {}}
+    library.each do |meaning, emoticons|
+      result["get_meaning"][emoticons[1]] = meaning
+      result["get_emoticon"][emoticons[0]] = emoticons[1]
+    end
+    result
+  end
+
+def get_japanese_emoticon(emoticon_file, en_emoticon)
+  library = load_library(emoticon_file)
+   if library["get_emoticon"].include? (en_emoticon)
+     library["get_emoticon"][en_emoticon]
+   else
+     "Sorry, that emoticon was not found"
+   end
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
+def get_english_meaning(emoticon_file, ja_emoticon)
+  library = load_library(emoticon_file)
+   if library["get_meaning"].include? (ja_emoticon)
+     library["get_meaning"][ja_emoticon]
+   else
+     "Sorry, that emoticon was not found"
+   end
 end
