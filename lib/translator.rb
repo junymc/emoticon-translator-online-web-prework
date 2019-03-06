ab050b9b6c9ba3fb53584a2e09609230adf76e70
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-06 21:25:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,6 +6,7 @@ def load_library(emoticon_file)
   library.each do |meaning, emoticons|
     result["get_meaning"][emoticons[1]] = meaning
     result["get_emoticon"][emoticons[0]] = emoticons[1]
+    binding.pry
   end
   result
 
